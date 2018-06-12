b6acd338278677630ed4f3890897e7b25a6568f4
mishu <mishu@xtelecom.ro>
2018-06-12 07:21:36 +0300

    fixes routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,7 +4,7 @@ Route::middleware(['web', 'auth', 'core'])
     ->prefix('api/howTo')->as('howTo.')
     ->namespace('LaravelEnso\HowToVideos\app\Http\Controllers')
     ->group(function () {
-        Route::resource('videos', 'VideoController', ['except' => ['create', 'edit', 'index']]);
+        Route::resource('videos', 'VideoController', ['except' => ['create', 'edit']]);
         Route::resource('tags', 'TagController', ['except' => ['show', 'create', 'edit']]);
         Route::resource('posters', 'PosterController', ['only' => ['store', 'show', 'destroy']]);
     });

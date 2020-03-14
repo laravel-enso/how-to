1aec39384c2273b7856df6dca1f1ca5bd90ac4e8
mishu <mishu@xtelecom.ro>
2020-03-14 18:29:46 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_149750_create_structure_for_how_to.php
+++ src/database/migrations/2017_01_01_149750_create_structure_for_how_to.php
@@ -1,25 +1,24 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForHowTo extends Migration
 {
     protected $permissions = [
-        ['name' => 'howTo.videos.index', 'description' => 'How to videos index page', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'howTo.videos.show', 'description' => 'Play video', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'howTo.videos.store', 'description' => 'Upload how to video', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'howTo.videos.update', 'description' => 'Update how to video', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'howTo.videos.destroy', 'description' => 'Delete how to video', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'howTo.videos.index', 'description' => 'How to videos index page', 'is_default' => true],
+        ['name' => 'howTo.videos.show', 'description' => 'Play video', 'is_default' => true],
+        ['name' => 'howTo.videos.store', 'description' => 'Upload how to video', 'is_default' => false],
+        ['name' => 'howTo.videos.update', 'description' => 'Update how to video', 'is_default' => false],
+        ['name' => 'howTo.videos.destroy', 'description' => 'Delete how to video', 'is_default' => false],
 
-        ['name' => 'howTo.tags.index', 'description' => 'Tags index page', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'howTo.tags.store', 'description' => 'Store new tag', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'howTo.tags.update', 'description' => 'Update tag', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'howTo.tags.destroy', 'description' => 'Delete tag', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'howTo.tags.index', 'description' => 'Tags index page', 'is_default' => true],
+        ['name' => 'howTo.tags.store', 'description' => 'Store new tag', 'is_default' => false],
+        ['name' => 'howTo.tags.update', 'description' => 'Update tag', 'is_default' => false],
+        ['name' => 'howTo.tags.destroy', 'description' => 'Delete tag', 'is_default' => false],
 
-        ['name' => 'howTo.posters.store', 'description' => 'Store new poster', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'howTo.posters.show', 'description' => 'Show poster', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'howTo.posters.destroy', 'description' => 'Delete poster', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'howTo.posters.store', 'description' => 'Store new poster', 'is_default' => false],
+        ['name' => 'howTo.posters.show', 'description' => 'Show poster', 'is_default' => true],
+        ['name' => 'howTo.posters.destroy', 'description' => 'Delete poster', 'is_default' => false],
     ];
 
     protected $menu = [

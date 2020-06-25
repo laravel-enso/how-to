3183f0c081cb4f4374ecf26b739f3da9943e4d27
mishu <mishu@xtelecom.ro>
2020-06-25 14:28:28 +0300

    removes storage folder that is now managed by core

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -9,33 +9,10 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->load()
-            ->mapMorphs()
-            ->publish();
-    }
-
-    private function load()
-    {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
 
-        return $this;
-    }
-
-    private function mapMorphs()
-    {
         Video::morphMap();
-
-        return $this;
-    }
-
-    private function publish()
-    {
-        $this->publishes([
-            __DIR__.'/../storage/app' => storage_path('app'),
-        ], 'howToVideos-storage');
-
-        return $this;
     }
 }

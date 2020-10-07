b33430760aeae122b369e13a70fbc21ca335c155
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:16:38 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/howTo')->as('howTo.')
-    ->namespace('LaravelEnso\HowTo\Http\Controllers')
     ->group(function () {
         require 'app/videos.php';
         require 'app/posters.php';
--- routes/app/tags.php
+++ routes/app/tags.php
@@ -1,13 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\HowTo\Http\Controllers\Tag\Destroy;
+use LaravelEnso\HowTo\Http\Controllers\Tag\Index;
+use LaravelEnso\HowTo\Http\Controllers\Tag\Store;
+use LaravelEnso\HowTo\Http\Controllers\Tag\Update;
 
-Route::namespace('Tag')
-    ->prefix('tags')
+Route::prefix('tags')
     ->as('tags.')
     ->group(function () {
-        Route::get('', 'Index')->name('index');
-        Route::post('', 'Store')->name('store');
-        Route::delete('{tag}', 'Destroy')->name('destroy');
-        Route::patch('{tag}', 'Update')->name('update');
+        Route::get('', Index::class)->name('index');
+        Route::post('', Store::class)->name('store');
+        Route::delete('{tag}', Destroy::class)->name('destroy');
+        Route::patch('{tag}', Update::class)->name('update');
     });

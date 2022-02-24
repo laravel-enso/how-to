a946de9bc0e5961186fc5a4aa528c92931157ff2
Manuela Mindroc <manuela.mindroc@earthlink.ro>
2022-02-24 16:58:52 +0200

    drops suffix for validators

--- src/Http/Controllers/Tag/Store.php
+++ src/Http/Controllers/Tag/Store.php
@@ -3,12 +3,12 @@
 namespace LaravelEnso\HowTo\Http\Controllers\Tag;
 
 use Illuminate\Routing\Controller;
-use LaravelEnso\HowTo\Http\Requests\ValidateTagRequest;
+use LaravelEnso\HowTo\Http\Requests\ValidateTag;
 use LaravelEnso\HowTo\Models\Tag;
 
 class Store extends Controller
 {
-    public function __invoke(ValidateTagRequest $request, Tag $tag)
+    public function __invoke(ValidateTag $request, Tag $tag)
     {
         return $tag->create($request->validated());
     }
